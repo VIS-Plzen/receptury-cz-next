7b8e56f34bc75d89e11e73cd3c09b6092dccdabb
ezyxels <ezyxels@gmail.com>
2023-12-10 12:48:03 +0100

    paginator v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Container from "@/components/ui/Container";
 import Heading from "@/components/ui/Heading";
+import Paginator from "@/components/ui/Paginator";
+import { useState } from "react";
 
-export default function Home() {
+export default async function Home() {
+  //const res = await backendWorker("12345VIS", "Test");
   return (
     <div className="flex flex-col items-stretch justify-start gap-24 py-32 md:py-48">
+      <PaginatorTester />
       <Container className="flex items-center justify-center rounded-3xl bg-primary-600 py-52 text-center">
         <Heading as={"h2"} size="xl" className="text-primary-50">
           Sekce Inspirace na vaření
@@ -27,6 +32,24 @@ export default function Home() {
           Volitelný obsah
         </Heading>
       </Container>
+    </div>
+  );
+}
+function PaginatorTester() {
+  const [page, setPage] = useState<number>(1);
+  const totalPages = 47;
+  return (
+    <div className="mx-auto flex flex-col">
+      <Heading size="2xl" className="mb-5 text-center">
+        {page}
+      </Heading>
+      <Paginator
+        currentPage={page}
+        totalPages={totalPages}
+        onArrowLeft={() => setPage(page - 1)}
+        onArrowRight={() => setPage(page + 1)}
+        onPageClick={(newPage: number) => setPage(newPage)}
+      />
     </div>
   );
 }
