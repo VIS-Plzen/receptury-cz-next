c821c5a145b7ea69ffbedf4e196cfed16dddc396
adambardzak <104588473+adambardzak@users.noreply.github.com>
2023-12-11 09:05:35 +0100

    cards and view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Container from "@/components/ui/Container";
 import Heading from "@/components/ui/Heading";
+import RecipeCardsGrid from "@/components/ui/RecipeCardsGrid";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-stretch justify-start gap-24 py-32 md:py-48">
-      <Container className="flex items-center justify-center rounded-3xl bg-primary-600 py-52 text-center">
-        <Heading as={"h2"} size="xl" className="text-primary-50">
-          Sekce Inspirace na vaření
-        </Heading>
+      <Container className="flex justify-center rounded-3xl py-52">
+        <RecipeCardsGrid />
       </Container>
 
       <Container className="flex items-center justify-center rounded-3xl bg-secondary-600 py-52 text-center">
