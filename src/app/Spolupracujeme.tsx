55522a91461a56d64337d457301b08af66142154
ezyxels <ezyxels@gmail.com>
2024-01-13 19:02:07 +0100

    spolupracujeme odkazy

--- src/app/Spolupracujeme.tsx
+++ src/app/Spolupracujeme.tsx
@@ -6,9 +6,11 @@ export default function Spolupracujeme({}) {
   return (
     <Container>
       <Heading>Spolupracujeme</Heading>
-      <div className="my-8 grid grid-cols-3 gap-x-5 gap-y-8 md:grid-cols-6">
+      <div className="my-8 grid grid-cols-2 gap-x-5 gap-y-8 sm:grid-cols-3 md:grid-cols-6">
         {Array.from({ length: 12 }, (_, index) => (
-          <Logo className="w-full" key={"safl" + index} />
+          <a href={`#${index}`} target="_blank" rel="noopener noreferrer">
+            <Logo className="w-full" key={"safl" + index} />
+          </a>
         ))}
       </div>
     </Container>
