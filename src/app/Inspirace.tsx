1d55e1654b84b169747877e4e2f2e99a1d8d124a
ezyxels <ezyxels@gmail.com>
2024-01-13 18:49:30 +0100

    úprava inspirace

--- src/app/Inspirace.tsx
+++ src/app/Inspirace.tsx
@@ -28,47 +28,37 @@ export default function Inspirace() {
   }
 
   return (
-    <Container
-      className={`${
-        hidden &&
-        "absolute left-1/2 top-28 -translate-x-1/2 -translate-y-full md:top-32"
-      }`}
-    >
+    <Container>
       <div className="flex flex-row items-center justify-between">
-        <Heading as="h1" size="lg" className={`${hidden && "hidden"}`}>
+        <Heading as="h1" size="lg">
           Inspirace na vaření
         </Heading>
-        <HideButton className="ml-auto flex md:hidden" />
+        <HideButton className="ml-auto flex" />
       </div>
-      <div
-        className={`flex w-full items-center justify-between ${
-          !hidden && " pt-5 md:pt-20"
-        }`}
-      >
-        <Tabs
-          defaultValue={selected}
-          className={`w-full ${hidden && "hidden"}`}
-          onValueChange={(value: string) => setSelected(value)}
-        >
-          <div className="flex w-full flex-row justify-between">
-            <TabsList className="flex w-full items-center justify-evenly md:max-w-[550px]">
-              <TabsTrigger value="recommended" className="w-full">
-                Doporučené pro vás
-              </TabsTrigger>
-              <TabsTrigger value="favorites" className="w-full">
-                Oblíbené
-              </TabsTrigger>
-              <TabsTrigger value="new" className="w-full">
-                Nové recepty
-              </TabsTrigger>
-            </TabsList>
-          </div>
-        </Tabs>
-        <HideButton className="ml-auto hidden md:flex" />
-      </div>
-      {!hidden && (
+      <div className={`${hidden && "hidden"}`}>
+        <div className="flex w-full items-center justify-between pt-5 md:pt-20">
+          <Tabs
+            defaultValue={selected}
+            className="w-full"
+            onValueChange={(value: string) => setSelected(value)}
+          >
+            <div className="flex w-full flex-row justify-between">
+              <TabsList className="flex w-full items-center justify-evenly md:max-w-[550px]">
+                <TabsTrigger value="recommended" className="w-full">
+                  Doporučené pro vás
+                </TabsTrigger>
+                <TabsTrigger value="favorites" className="w-full">
+                  Oblíbené
+                </TabsTrigger>
+                <TabsTrigger value="new" className="w-full">
+                  Nové recepty
+                </TabsTrigger>
+              </TabsList>
+            </div>
+          </Tabs>
+        </div>
         <RecipeCardsGrid length={12} gridView className="flex flex-row" />
-      )}
+      </div>
     </Container>
   );
 }
