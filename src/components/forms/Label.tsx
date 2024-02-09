60089520e55d7ca55e093764257960abebc1a2bc
adambardzak <104588473+adambardzak@users.noreply.github.com>
2024-02-09 09:46:50 +0100

    remove input floating labels

--- src/components/forms/Label.tsx
+++ src/components/forms/Label.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { cn } from "@/utils/cn";
 import { Slot } from "@radix-ui/react-slot";
 import { forwardRef } from "react";
@@ -12,10 +11,19 @@ type Props = React.ComponentPropsWithoutRef<"label"> & {
   [key: string]: any;
 };
 
+// Component Variants
+// const componentVariants = {
+// const cv = {
+//   base: "",
+//   prop: {
+//     value: "",
+//     value: "",
+//     value: "",
+//   },
+// }
 const Label = forwardRef<HTMLLabelElement, Props>(
   ({ htmlFor, children, asChild, className = "", ...props }, forwardedRef) => {
     const Component = asChild ? Slot : "label";
-
     return (
       <Component
         ref={forwardedRef}
@@ -24,14 +32,7 @@ const Label = forwardRef<HTMLLabelElement, Props>(
           // Prevent double click selection
           if (!e.defaultPrevented && e.detail > 1) e.preventDefault();
         }}
-        className={cn(
-          "cursor-text font-medium leading-none",
-          "absolute left-4 duration-75",
-          "peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:font-normal peer-placeholder-shown:text-current",
-          "top-0 -translate-y-1/2 rounded-md bg-white px-1 text-sm font-bold text-black",
-          "peer-focus:top-0 peer-focus:-translate-y-1/2 peer-focus:text-base peer-focus:font-bold peer-focus:text-black",
-          className
-        )}
+        className={cn("text-base font-medium leading-none", className)}
         {...props}
       >
         {children}
@@ -39,6 +40,5 @@ const Label = forwardRef<HTMLLabelElement, Props>(
     );
   }
 );
-
 Label.displayName = "Label";
 export default Label;
