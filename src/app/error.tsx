233ab63769f0fddccdb014dec4548f9b79c62a9e
fandablazek97 <blazek127@gmail.com>
2024-05-30 10:29:51 +0200

    added error.tsx + not-found.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,33 @@
+"use client"; // Error components must be Client Components
+
+import Container from "@/components/ui/Container";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="py-10">
+      <Container className="bg-error-400 py-10 text-error-950">
+        <h2>Something went wrong!</h2>
+        <button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+      </Container>
+    </div>
+  );
+}
